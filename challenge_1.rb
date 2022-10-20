90d5359ecb0fe6062a9db6d0148747983551a6ba
Kassandra Leyba <kassandra.leyba@gmail.com>
2022-10-19 20:08:58 -0600

    Add challenge_1 solution

---
+++ challenge_1.rb
@@ -0,0 +1,8 @@
+# Challenge 1
+# Use the each method to print out a complete sentence stating the weather using the Array below:
+
+weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+
+weather_options.each do |weather_options|
+    puts "Today the weather will be #{weather_options}."
+end
